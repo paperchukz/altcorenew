0c8bb05ded53cce6945e28014a75e308b5cccfd9
paperchukz <paperchukz@gmail.com>
2018-07-31 12:15:55 +0700

    new revisi

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
-$(document).ready(function() {
-   $('.mdb-select').material_select();
- });
+// $(document).ready(function() {
+// 	setTimeout(function(){
+// 		$('.index-page nav').css('visibility','visible').hide().fadeIn('slow');
+//   }, 500);
+//   setTimeout(function(){
+//   	$('.index-page .banner').css('visibility', 'visible').hide().fadeIn('slow');	
+//   }, 800);
+// 	setTimeout(function(){
+// 		$(".index-page .section1 h2").css('visibility', 'visible').hide().fadeIn(1000);
+// 		$(".index-page .section1 h2").animate({top: 0}, {queue: false, duration: 1000}); 
+// 	}, 1100);
+// 	setTimeout(function(){
+// 		$(".index-page .section1 h5").css('visibility', 'visible').hide().fadeIn(1000);
+// 		$(".index-page .section1 h5").animate({top: 0}, {queue: false, duration: 1000}); 
+// 	}, 1400);
+
+// 	$(window).scroll(function(){
+// 		if($(window).scrollTop()>=850){
+// 	setTimeout(function(){
+// 		$(".index-page .section2 .section-text").css('visibility', 'visible').hide().fadeIn(1000);
+// 		$(".index-page .section2 .section-text").animate({left: 0}, {queue: false, duration: 1000}); 
+// 	}, 500);
+// 	setTimeout(function(){
+// 		$(".index-page .section2 .section-img").css('visibility', 'visible').hide().fadeIn(1000);
+// 		$(".index-page .section2 .section-img").animate({right: 0}, {queue: false, duration: 1000}); 
+// 	}, 800);
+// 	setTimeout(function(){
+// 		$(".index-page .section3 .section-img").css('visibility', 'visible').hide().fadeIn(1000);
+// 		$(".index-page .section3 .section-img").animate({left: 0}, {queue: false, duration: 1000}); 
+// 	}, 1100);
+// 	setTimeout(function(){
+// 		$(".index-page .section3 .section-text").css('visibility', 'visible').hide().fadeIn(1000);
+// 		$(".index-page .section3 .section-text").animate({right: 0}, {queue: false, duration: 1000}); 
+// 	}, 1400);
+// 	}
+// 	});
+// });
